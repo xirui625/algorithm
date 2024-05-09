e7a1824c91d8463374eba696348de10f6797215a
Siri_ii <335683658@qq.com>
2024-05-09 10:16:47 +0800

    修改代码

--- 其他/最大数.py
+++ 其他/最大数.py
@@ -7,4 +7,21 @@
 @Version :   1.0
 @Desc    :   None
 '''
+# 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
 
+# 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
+
+from functools import cmp_to_key
+
+class Solution:
+    def helper(self, num1, num2):
+        # 将两个数字转化为字符串，并比较拼接后的大小
+        if int(str(num1) + str(num2)) > int(str(num2) + str(num1)):
+            return -1
+        else:
+            return 1
+
+    def largestNumber(self, nums: List[int]) -> str:
+        nums.sort(key=cmp_to_key(self.helper))
+        ret = ''.join(map(str, nums))
+        return '0' if ret[0] == '0' else ret
