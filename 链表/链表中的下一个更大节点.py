0669db138208654a1a97f32628c5dc147b1d6e2d
Siri_ii <335683658@qq.com>
2024-05-11 10:46:38 +0800

    代码修改

--- 链表/链表中的下一个更大节点.py
+++ 链表/链表中的下一个更大节点.py
@@ -16,25 +16,43 @@
 class Solution:
     # 206. 反转链表
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 初始化前一个节点为空
         pre = None
+        # 当前节点指向链表的头节点
         cur = head
+        # 循环遍历链表节点
         while cur:
+            # 记录当前节点的下一个节点
             nxt = cur.next
+            # 将当前节点的next指针指向前一个节点
             cur.next = pre
+            # 更新前一个节点为当前节点
             pre = cur
+            # 更新当前节点为下一个节点
             cur = nxt
+        # 返回反转后的头节点
         return pre
 
     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+        # 将链表反转
         head = self.reverseList(head)
+        # 初始化答案列表
         ans = []
-        st = []  # 单调栈（节点值）
+        # 初始化单调栈（节点值）
+        st = []  
+        # 遍历反转后的链表
         while head:
+            # 移除单调栈中小于当前节点值的节点
             while st and st[-1] <= head.val:
-                st.pop()  # 弹出无用数据
-            ans.append(st[-1] if st else 0)  # 栈顶就是第 i 个节点的下一个更大元素
+                st.pop()  
+            # 栈顶就是第 i 个节点的下一个更大元素
+            ans.append(st[-1] if st else 0)  
+            # 将当前节点值加入单调栈
             st.append(head.val)
+            # 移动到下一个节点
             head = head.next
-        return ans[::-1]  # 由于是倒着记录答案的，返回前要把答案反转
+        # 由于是倒着记录答案的，返回前要把答案反转
+        return ans[::-1]
+
 
         
--- 链表/链表加1.py
+++ 链表/链表加1.py
@@ -10,44 +10,36 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
-    # def plusOne(self , head ):
-    #     stack1 = []
-    #     stack2 = []
-    #     while head:
-    #         stack1.append(head.val)
-    #         head = head.next
-    #     top = stack1.pop() + 1
-    #     ca = int(top / 10)
-    #     stack2.append(int(top % 10))
-    #     while stack1:
-    #         num = stack1.pop()
-    #         total = num + ca
-    #         ca = int(total / 10)
-    #         stack2.append(int(total % 10))
-    #     if ca:
-    #         stack2.append(ca)
-    #     newHead = newTail = ListNode(stack2.pop())
-    #     while stack2:
-    #         newTail.next = ListNode(stack2.pop())
-    #         newTail = newTail.next
-    #     return newHead
-    class Solution(object):
-        def plusOne(self, head):
-            st = []
-            while head:
-                st.append(head.val)
-                head = head.next
-            carry = 0
-            dummy = ListNode(0)
-            adder = 1
-            while st or adder != 0 or carry > 0:
-                digit = st.pop() if st else 0
-                sum = digit + adder + carry
-                carry = 1 if sum >= 10 else 0
-                sum = sum - 10 if sum >= 10 else sum
-                cur = ListNode(sum)
-                cur.next = dummy.next
-                dummy.next = cur
-                adder = 0
-            return dummy.next
+class Solution(object):
+    def plusOne(self, head):
+        # 用于存储链表中的节点值
+        st = []
+        # 遍历链表，将节点值存入列表中
+        while head:
+            st.append(head.val)
+            head = head.next
+        # 初始化进位和虚拟头节点
+        carry = 0
+        dummy = ListNode(0)
+        # 初始要加的数为1
+        adder = 1
+        # 当链表还有节点值未处理、加的数不为0或有进位时，循环执行
+        while st or adder != 0 or carry > 0:
+            # 如果列表不为空，弹出当前节点值；否则当前值为0
+            digit = st.pop() if st else 0
+            # 计算当前位的和，包括当前节点值、加的数以及进位
+            sum = digit + adder + carry
+            # 更新进位，如果当前位的和大于等于10，则进位为1，否则为0
+            carry = 1 if sum >= 10 else 0
+            # 如果当前位的和大于等于10，则当前值需要减去10，否则不变
+            sum = sum - 10 if sum >= 10 else sum
+            # 创建新的节点，值为当前位的和
+            cur = ListNode(sum)
+            # 将新节点的下一个节点指向结果链表的头节点
+            cur.next = dummy.next
+            # 将虚拟头节点的下一个节点指向新节点，新节点成为结果链表的新的头节点
+            dummy.next = cur
+            # 更新加的数为0，因为已经加过了
+            adder = 0
+        # 返回结果链表的头节点
+        return dummy.next
